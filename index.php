362ed5f0856608eb585770f127bf8d3f1c8ca9e4
elbakyanit@gmail.com <elbakyanit@gmail.com>
2020-11-01 13:42:49 +0400

    add php

--- index.php
+++ index.php
@@ -7,12 +7,15 @@
     <link rel="stylesheet" href="./style/normalize.css">
     <link rel="stylesheet" href="./style/all.min.css">
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./header/header.css">
+    <link rel="stylesheet" href="./section-1/section-1.css">
     <script src="./script/all.min.js"></script>
 </head>
 <body>
     <div class="wrapper">
         <div class="container">
-            
+            <?php include "./header/header.html" ?>
+            <?php include "./section-1/section-1.html" ?>
         </div>
     </div>
 </body>
